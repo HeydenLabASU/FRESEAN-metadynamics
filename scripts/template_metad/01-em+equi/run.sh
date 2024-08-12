3778769ba93cb14e3636ae3b2a3d72feba823119
Matthias Heyden <95201808+HeydenLabASU@users.noreply.github.com>
2024-08-12 16:19:21 -0700

    Add files via upload

---
+++ scripts/template_metad/01-em+equi/run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -G a100:1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH -t 0-04:00                  # wall time (D-HH:MM)
+#SBATCH -J EMEQUI
+
+#BEGIN INPUT
+inpTOP=../00-prep/topol.top
+inpGRO=../00-prep/prep.gro
+next=1
+nextDir=../02-MD
+gmx=gmx_plumed
+#END INPUT
+
+files=(
+em.mdp
+equi.mdp
+${inpTOP}
+${inpGRO}
+)
+for file in ${files[@]}
+do
+if [ ! -f ${file} ]; then
+echo "-could not find file ${file} in current directory"
+echo "-exiting"
+exit
+fi
+done
+
+#Run energy minimization of the freshly solvated protein
+if [ ! -d em ]; then
+mkdir em
+fi
+cd em
+$gmx grompp -f ../em.mdp -c ../${inpGRO} -p ../${inpTOP} -r ../${inpGRO} -o em.tpr >& grompp.out
+$gmx mdrun -v -deffnm em -cpi -nt 1 -pin on >& mdrun.out
+cd ..
+
+#Run equilibration MD simulation with position restraints on heavy protein atoms
+if [ ! -d equi ]; then
+mkdir equi
+fi
+cd equi
+$gmx grompp -f ../equi.mdp -c ../em/em.gro -p ../${inpTOP} -r ../${inpGRO} -o equi.tpr >& grompp.out
+export OMP_NUM_THREADS=16
+$gmx mdrun -v -deffnm equi -cpi -ntomp 16 -pin on -pme gpu -gpu_id 0 >& mdrun.out
+cd ..
+
+#Start next part of the project if next=1
+if [ ${next} -eq 1 ]; then
+  if [ -f equi/equi.gro ]; then
+    if [ -d ${nextDir} ]; then
+      curDir=`pwd`
+      cd ${nextDir}
+      if [ -f run.sh ]; then
+        sbatch run.sh
+      fi
+      cd ${curDir}
+    fi
+  fi
+fi
---
+++ scripts/template_metad/03-CG/run.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -t 0-04:00                  # wall time (D-HH:MM)
+#SBATCH -J COARSE
+
+#BEGIN INPUT
+gmx=gmx_plumed
+#GMX topology for protein
+inpTOPprot=../00-prep/topol_prot.top
+#GMX files from MD sampling
+inpTPR=../02-MD/sample-NPT.tpr
+inpTRR=../02-MD/sample-NPT.trr
+#output trajectory for protein with PBC fixes
+outTRRprotAA=../02-MD/sample-NPT_prot_pbc.trr
+#output trajectory for protein in CG representation
+outTRRprotCG=sample-NPT_prot_pbc-CG.trr
+#GMX group number for protein (gmx trjconv)
+outGrp=1
+#GMX itp file with nonbonded parameters
+GMXDATA=$HOME/gromacs-2022.5-plumed-2.8.2
+ffnonbonded=$GMXDATA/share/top/amber99sb-ildn.ff/ffnonbonded.itp
+#time between frames in input trajectory
+TRJtimestep=0.020
+#number of steps in input trajectory
+TRJframes=1000000
+next=1
+nextDir=../04-FRESEAN
+#END INPUT
+
+files=(
+${inpTOPprot}
+${inpTPR}
+${inpTRR}
+${ffnonbonded}
+static.job
+)
+for file in ${files[@]}
+do
+if [ ! -f ${file} ]; then
+echo "-could not find file ${file} in current directory"
+echo "-exiting"
+exit
+fi
+done
+
+#Generate a all-atom topology file (custom format) for the protein
+fresean mtop -p ${inpTOPprot} << STOP >& mtop.out
+${inpTOPprot}
+${inpTOPprot}
+${ffnonbonded}
+topol_prot-aa.mtop
+STOP
+
+#Make the protein whole (fix PBC jumps) and write out protein-only trajectory
+$gmx trjconv -s ${inpTPR} -f ${inpTRR} -o ${outTRRprotAA} -pbc mol << STOP >& trjconv1.out
+${outGrp}
+STOP
+
+#Generate all-atom reference structure for rot+trans fitting
+$gmx trjconv -s ${inpTPR} -f ${outTRRprotAA} -o ref.gro -e 0.0 << STOP >& trjconv2.out
+${outGrp}
+STOP
+#some postprocessing for PLUMED compatibility of ref.pdb
+$gmx editconf -f ref.gro -o ref.pdb
+tail -n +5 ref.pdb > tmp.pdb
+head -n -2 tmp.pdb > ref.pdb
+sed -i '1i REMARK TYPE=OPTIMAL' ref.pdb
+sed -i '$aEND' ref.pdb
+rm tmp.pdb
+
+#Generate input parameter file for coarse-graining
+#WARNING: update expected soon
+cat << STOP >& coarse.inp
+#fnTop
+topol_prot-aa.mtop
+#fnCrd
+${outTRRprotAA}
+#fnVel (if format xyz,crd,dcd)
+#fnJob
+static.job
+#grp
+0
+#nRead
+${TRJframes}
+#nSample
+1
+#fnOutTraj
+tmptraj.gro
+#fnOutTopol
+topol_prot-cg.mtop
+STOP
+#Apply coarse-graining to all-atom protein trajectory
+fresean coarse -f coarse.inp >& coarse.out
+#Convert coarse-grained trajectory from gro (ASCII text) to trr (binary) format
+$gmx trjconv -s tmptraj.gro -f tmptraj.gro -timestep ${TRJtimestep} -o ${outTRRprotCG} << STOP >& trjconv3.out
+0
+STOP
+
+#Generate coarse-grained reference structure for rot+trans fitting
+nAtoms=`head -n 2 tmptraj.gro | tail -n 1`
+nLines=${nAtoms}
+((nLines +=3))
+head -n ${nLines} tmptraj.gro >& ref-cg.gro
+rm tmptraj.gro
+
+#Start next part of the project if next=1
+if [ ${next} -eq 1 ]; then
+  if [ -f ${outTRRprotCG} ]; then
+    if [ -d ${nextDir} ]; then
+      curDir=`pwd`
+      cd ${nextDir}
+      if [ -f run.sh ]; then
+        sbatch run.sh
+      fi
+      cd ${curDir}
+    fi
+  fi
+fi
